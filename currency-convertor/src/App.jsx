00fa0ade52275d67908116cfc155d25cdfd46784
Kapil Raut <kapilraut026@gmail.com>
2025-04-25 22:11:41 +0545

    !111!

---
+++ currency-convertor/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+    style={{backgroundImage: `url(https://images.pexels.com/photos/259027/pexels-photo-259027.jpeg)`}}>
+      <h1 className="bg-red-200">Test for </h1>
+    </div>
+</>
+  )
+}
+
+export default App
